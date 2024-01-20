1640210d161eb1c487971f242626d791e650428f
neiltron <neil@descend.org>
2024-01-20 01:04:13 -0500

    update sender/receiver UI

--- src/components/AudioReceiver.vue
+++ src/components/AudioReceiver.vue
@@ -1,20 +1,32 @@
 <template>
   <div>
-    <h2>Data Receiver</h2>
-    <button @click="toggleReceiving">
-        {{ isReceiving ? 'Receiving' : 'Receive' }}
-    </button>
-    <textarea v-model="receivedText" readonly placeholder="Received data will appear here"></textarea>
+    <div class="text-display">
+      <button @click="toggleReceiving">
+          {{ isReceiving ? 'Receiving' : 'Receive' }}
+      </button>
+      <span class="text-display-inner">
+        {{ fullText }}
+      </span>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onBeforeUnmount, ref } from 'vue';
+import { nextTick, onBeforeUnmount, ref, watch } from 'vue';
 import AudioDataReceiver from '../lib/audiodatareceiver';
 
 const audioDataReceiver = new AudioDataReceiver();
 const receivedText = ref('');
+const fullText = ref('');
 const isReceiving = ref(audioDataReceiver.isReceiving);
+const textareaEl = ref(null);
+
+watch(receivedText, async () => {
+  await nextTick();
+  if (textareaEl.value != null) {
+    textareaEl.value.scrollTop = textareaEl.value?.scrollHeight;
+  }
+});
 
 const toggleReceiving = () => {
     if (isReceiving.value) {
@@ -31,7 +43,8 @@ const stopReceiving = () => {
 const startReceiving = () => {
   audioDataReceiver.startReceiving(
     (text: string) => {
-        receivedText.value = text;
+      receivedText.value = text;
+      fullText.value += text;
     },
     (status: boolean) => {
         isReceiving.value = status;
@@ -39,7 +52,11 @@ const startReceiving = () => {
 };
 
 onBeforeUnmount(() => {
-  audioDataReceiver.stopReceiving();
+  try {
+    audioDataReceiver.stopReceiving();
+  } catch (e) {
+    console.error(e);
+  }
 });
 </script>
 
@@ -47,12 +64,33 @@ onBeforeUnmount(() => {
 h2 {
     padding: 0;
 }
-button { display: block; }
+button {
+  display: block;
+  padding: 8px 2px;
+  border-radius: 6px;
+}
 textarea {
     width: 30rem;
     min-height: 10rem;
     font-size: 1rem;
     padding: .4rem;
     margin-top: .5rem;
+}
+
+.text-display {
+  height: 36rem;
+  font-size: 2rem;
+  font-weight: 800;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  text-align: center;
+  padding: 20px;
+  max-width: 100%;
+  background-color: #222;
+  border-radius: 6px;
+  min-height: 20rem;
+  display: flex;
+  flex-direction: column;
+  text-transform: uppercase;
+  word-break: break-all;
 }
 </style>
--- src/components/AudioSender.vue
+++ src/components/AudioSender.vue
@@ -1,13 +1,12 @@
 <template>
-        <div>
-            <h2>Data sender</h2>
-            <textarea v-model="textToSend" placeholder="Enter text to send"></textarea>
-            <button @click="startSending">Send</button>
-        </div>
+    <div>
+        <textarea v-model="textToSend" placeholder="Enter text to send"></textarea>
+        <button @click="startSending">Send</button>
+    </div>
 </template>
-  
+
 <script lang="ts">
-import AudioDataSender from "../lib/audiodatasender"; 
+import AudioDataSender from "../lib/audiodatasender";
 
 export default {
     data() {
@@ -26,15 +25,27 @@ export default {
 
 <style scoped>
 /* Add your component-specific styles here */
+div {
+    display: flex;
+    flex-direction: column;
+}
 textarea {
-    width: 30rem;
+    flex: 1;
+    width: 100%;
     min-height: 10rem;
     font-size: 1rem;
-    padding: .4rem;
+    padding: 1rem;
     margin-bottom: 0;
     display: block;
+    border-radius: 6px;
+    background-color: #333;
+    color: #fff;
+    border: none;
+    font-size: 2rem;
 }
 button {
-    margin-bottom: .5rem;
+    margin-top: 8px;
+    padding: 8px 2px;
+    border-radius: 2px;
 }
 </style>
